c3423e4ecd805ed562735702e2a8fc532d8d30ca
Eral02 <122754482+Eral02@users.noreply.github.com>
2024-08-24 15:49:10 +0500

    Add files via upload

---
+++ Assignment 05.py
@@ -0,0 +1,15 @@
+a=int(input("Enter the first num: "))
+b=int(input("Enter the second num: "))
+c=int(input("Enter the third num: "))
+
+print("Maximum num is: ",max)
+
+if  b<= a >= c:
+    print(a)
+    else:
+         a <= b >= c:
+        print(b)
+        else:
+          a <= c >= b:
+          print(c)
+          
